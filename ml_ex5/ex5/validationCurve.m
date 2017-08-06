2df714c51b3b46c04a1b75be42ff6cb945973954
Chingun <chingun@umich.edu>
2017-08-06 15:02:42 -0400

    Add files via upload

---
+++ ml_ex5/ex5/validationCurve.m
@@ -0,0 +1,51 @@
+function [lambda_vec, error_train, error_val] = ...
+    validationCurve(X, y, Xval, yval)
+%VALIDATIONCURVE Generate the train and validation errors needed to
+%plot a validation curve that we can use to select lambda
+%   [lambda_vec, error_train, error_val] = ...
+%       VALIDATIONCURVE(X, y, Xval, yval) returns the train
+%       and validation errors (in error_train, error_val)
+%       for different values of lambda. You are given the training set (X,
+%       y) and validation set (Xval, yval).
+%
+
+% Selected values of lambda (you should not change this)
+lambda_vec = [0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10]';
+
+% You need to return these variables correctly.
+error_train = zeros(length(lambda_vec), 1);
+error_val = zeros(length(lambda_vec), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return training errors in 
+%               s
+%
+% Note: You can loop over lambda_vec with the following:
+%
+%       for i = 1:length(lambda_vec)
+%           lambda = lambda_vec(i);
+%           % Compute train / val errors when training linear 
+%           % regression with regularization parameter lambda
+%           % You should store the result in error_train(i)
+%           % and error_val(i)
+%           ....
+%           
+%       end
+
+m = length(lambda_vec);
+length(X(1,:))
+theta = zeros(1, length(X(1,:)));
+
+for i = 1:m
+    lambda = lambda_vec(i);
+    theta =  trainLinearReg(X, y, lambda);
+    J(i) = linearRegCostFunction(X, y, theta, lambda);
+    error_train(i) = sum((1/(2*m))*(X*theta-y).^2);
+    error_val(i) = sum((1/(2*m))*(Xval*theta-yval).^2);
+    
+end
+
+
+% =========================================================================
+
+end
